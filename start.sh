dcce97a33b07142a5096b205fa87d504ab3ce6fa
AJ NOURI <ajn@localhost.localdomain>
2016-06-26 06:11:55 +0200

    Leave only nfs dir & clean up the image for use within GNS3.

--- start.sh
+++
@@ -1,8 +0,0 @@
-# eval "$(docker-machine env)"
-docker run -d \
-     -p 2049:2049 \
-     -v /home/docker:/export/docker \
-     --name nfs \
-     --hostname nfs \
-     --privileged \
-     pierophp/nfs
